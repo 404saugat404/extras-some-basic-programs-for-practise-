fb352d4cbcfae02e840a402ced08276c1e379649
saugat <saugat556513@gmail.com>
2024-03-19 19:50:05 +0545

    simple snake game day 1

---
+++ snake_game.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import random
+
+GAME_WIDTH=700
+GAME_HEIGHT=700
+SPEED=50
+SPACE_SIZE=50
+BODY_PARDS=3
+SNAKE_COLOR='#00FF00'
+FOOD_COLOR='#FF0000'
+BACKGROUND_COLOR='#000000'
+
+
+class snake:
+    pass
+
+class food:
+    pass
+
+def next_turn():
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
