72540f0b2c02e95ea9d213d7a4b5a030a4f5fcfa
saugat <saugat556513@gmail.com>
2024-08-16 00:03:47 +0545

    modified variable

--- leetcode/problem1.py
+++ leetcode/problem1.py
@@ -1,11 +1,22 @@
-def two_sum_problem(numbers,target):
-    dict={}
-    for i, number in enumerate(numbers):
-        value=target-number
-        dict[number]=i
-        if value in dict:
-            print("the numbers are: ",dict[value],i)
+# def two_sum_problem(numbers,target):
+#     dict={}
+#     for i, number in enumerate(numbers):
+#         value=target-number
+#         dict[number]=i
+#         if value in dict:
+#             print("the numbers are: ",dict[value],i)
     
-two_sum_problem([1,2,3,4,5],6)
+# two_sum_problem([1,2,3,4,5],6)
 
-#this need to be solved more
+
+def two_sum(list,target):
+    dict={}
+    for i , j in enumerate(list):
+        s=target-j
+        dict[j]=i
+        if s in dict:
+            print(f"{dict[s],i}") 
+        
+        
+l=[1,2,3,4]
+two_sum(l,6)
