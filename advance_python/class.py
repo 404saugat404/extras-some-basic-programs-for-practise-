7f335d616a9c3a0a03b0997db08a472360cb602d
saugat <saugat556513@gmail.com>
2024-08-23 14:15:08 +0545

    practise

---
+++ advance_python/class.py
@@ -0,0 +1,70 @@
+# #creating a class
+# class Myclass(object):
+#     def show_print(self):
+#         print("hello world!!!")
+
+
+# #creating object here, hello is object and myclass is class
+# #and we can access the method(function in simple language) of class using class.method(or function) name
+# hello=Myclass()
+# hello.show_print()
+
+
+
+
+# #creating class
+# class Mobile:
+#     def __init__(self) -> None:
+#         self.model="realme"
+
+#     def show_model(self):
+#         print("model name: " , self.model)
+
+
+# #creating object
+
+# realme=Mobile()
+# realme.show_model()
+
+# #you can also access the variable inside class and change it using object
+
+# readme=Mobile()
+# readme.model="samsung s23"
+# readme.show_model()
+
+
+# #creating class
+# class Mobile:
+#     def __init__(self) -> None:
+#         self.model="abc"
+
+#     def show_model(self,price):
+#         print("model: ",self.model,"price:",price)
+
+# #creating object
+# readme=Mobile()
+
+# readme.show_model(100)
+
+# #output: model:  abc price: 1000
+
+
+
+# #creating class
+# class Mobile:
+#     def __init__(self,m):
+#         self.model=m
+
+#     def show_model(self,price):
+#         print("model: ",self.model,"price:",price)
+
+# #creating object
+# readme=Mobile("readme")  #here we can pass the variable in class that is needed constructor method : constructor is automatically called when we create an object
+
+# readme.show_model(100)
+
+# #output: model:  readme price: 100
+
+
+
+"""revse video 11,15,16"""
