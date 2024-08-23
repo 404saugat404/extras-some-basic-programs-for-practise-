50a1c766f5b346ab8be888ee2f7192e09e6535ba
saugat <saugat556513@gmail.com>
2024-08-23 22:47:45 +0545

    day 9 of leetcoding

---
+++ leetcode/longest_substr_without_repeat_3.py
@@ -0,0 +1,22 @@
+def no_repeat(s):
+    dictionary={}
+    left=0
+    length=0
+
+    for right in range(len(s)):
+        char=s[right]
+        
+        if char in dictionary and dictionary[char]>=left:
+            left=dictionary[char]+1
+
+        else:
+            length=max(length,right-left+1)
+
+            
+
+
+
+
+
+        dictionary[char]=right
+
