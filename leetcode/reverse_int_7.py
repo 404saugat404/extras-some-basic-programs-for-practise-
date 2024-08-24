650c7ffcc2eeef86e0f54c2ee221881b9a760fd7
saugat <saugat556513@gmail.com>
2024-08-24 22:12:08 +0545

    day 10 of leetcoding

---
+++ leetcode/reverse_int_7.py
@@ -0,0 +1,17 @@
+def rev(integer_to_be_reversed):
+    if integer_to_be_reversed>0:
+        val=int(str(integer_to_be_reversed)[::-1])
+
+    else:
+        val=-(int(str(abs(integer_to_be_reversed))[::-1]))
+
+    if val<-2**31 or val>2**31-1:
+        return 0
+    
+    else:
+        return val
+    
+
+value=1245
+
+print(rev(value))
