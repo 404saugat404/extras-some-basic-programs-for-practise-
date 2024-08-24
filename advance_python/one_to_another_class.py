21f85020690ba83b958ca8d8a34d4baba771512a
saugat <saugat556513@gmail.com>
2024-08-24 23:25:42 +0545

    practising

---
+++ advance_python/one_to_another_class.py
@@ -0,0 +1,29 @@
+#here we will learn how to pass the method and parameters of one class to another class
+
+class ClassA:
+    def __init__(self,m,n):
+        self.name=m
+        self.roll=n
+
+    def disp(self):
+        print("name: ", self.name)
+        print("roll:",self.roll)
+
+
+#now lets pass the component of classA to another class
+
+class user:
+    @staticmethod
+
+    def show(s):
+        print("user name:",s.name)  #here name of s.name came from name and roll defined as self.name and self.roll
+
+        print("user roll:",s.roll)
+        
+
+
+objA=ClassA("saugat","39")
+
+user.show(objA)
+
+
