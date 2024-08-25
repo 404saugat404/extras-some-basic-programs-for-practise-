da0563cb4d39d2c87b3908001f6af029b8dcb69a
saugat <saugat556513@gmail.com>
2024-08-25 22:52:43 +0545

    inheritance

--- advance_python/inheretence.py
+++ advance_python/inheretence.py
@@ -1,2 +1,29 @@
 #advantage: it makes the code reusuable , ie: you dont need to write same code again and again
 
+class father:
+    money=5000
+
+    def show(self):
+        print("parent class instance method")
+
+    @classmethod
+
+    def showmoney(cls):
+        print("parent class class method", cls.money)
+
+class son(father):
+    def disp(self):
+        print("child class instance method")
+
+s=son()
+
+# s.disp()
+#output: child class instance method
+
+#now lets use child to access fathers method/function
+
+s.show()
+#output: parent class instance method
+
+s.showmoney()
+#output:parent class class method 5000
