2f17ec7bad2aeb9cffbe78d882ee890227ca4671
saugat <saugat556513@gmail.com>
2024-08-25 23:09:10 +0545

    constructor overriding

--- advance_python/constructor_and_inheritance.py
+++ advance_python/constructor_and_inheritance.py
@@ -2,6 +2,24 @@
 #ans is simple, the childs constructor will replace the parents construcotor.
 #lets see an example
 
+# class father:
+#     def __init__(self) -> None:
+#         self.money=1000
+#         print("fathers constructor: ", self.money)
+    
+#     def show(self):
+#         print("show money :", self.money)
+
+
+# class son(father):
+#     def son_show(self):
+#         print("son output")
+
+# s=son()
+#Output:fathers constructor: 1000
+#here since only one constructor there is , so child is showing fathers constructor
+
+
 class father:
     def __init__(self) -> None:
         self.money=1000
@@ -12,9 +30,14 @@ class father:
 
 
 class son(father):
+    def __init__(self) -> None:
+        self.money=5000
+        print("sons constructor: ", self.money)
+
+
     def son_show(self):
         print("son output")
 
 s=son()
-#Output:fathers constructor: 1000
-#here since only one constructor there is , so child is showing fathers constructor
+#output:sons constructor:  5000
+
