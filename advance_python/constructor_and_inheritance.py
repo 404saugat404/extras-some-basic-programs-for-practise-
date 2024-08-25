a01704b66281e269a19c8ab2cc83bf033615f1b4
saugat <saugat556513@gmail.com>
2024-08-25 23:08:09 +0545

    single constructor of father and child

---
+++ advance_python/constructor_and_inheritance.py
@@ -0,0 +1,20 @@
+#what if we create constructor in both child and parent class?which constructor will be called when we created the child object?
+#ans is simple, the childs constructor will replace the parents construcotor.
+#lets see an example
+
+class father:
+    def __init__(self) -> None:
+        self.money=1000
+        print("fathers constructor: ", self.money)
+    
+    def show(self):
+        print("show money :", self.money)
+
+
+class son(father):
+    def son_show(self):
+        print("son output")
+
+s=son()
+#Output:fathers constructor: 1000
+#here since only one constructor there is , so child is showing fathers constructor
