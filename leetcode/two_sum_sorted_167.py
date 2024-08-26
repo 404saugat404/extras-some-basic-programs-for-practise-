a0b516984dd4a9ba487fcc7afc9a7e6f4601cc16
saugat <saugat556513@gmail.com>
2024-08-26 14:48:55 +0545

    day 12 of leetcoding

---
+++ leetcode/two_sum_sorted_167.py
@@ -0,0 +1,23 @@
+def number(list, target):
+    left=0
+    right=len(list)-1
+
+    while left<right:
+        current_sum=list[left]+list[right]
+
+        if current_sum>target:
+            right=right-1
+
+        elif current_sum<target:
+            left=left-1
+
+        else:
+            return [left+1,right+1]
+        
+
+numbers = [2,7,11,15]
+target = 9
+
+print(number(numbers,target))
+
+#time complexity is 
