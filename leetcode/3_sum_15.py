1119836a3d9db86622d0e4d27310316189f1a947
saugat <saugat556513@gmail.com>
2024-08-27 14:58:50 +0545

    day 13 of leetcode

--- leetcode/3_sum_15.py
+++ leetcode/3_sum_15.py
@@ -1,24 +1,72 @@
+# def three_sum(list):
+#     h={}
+#     n=len(list)
+#     s=set()
+
+#     for i , num in enumerate(list):
+#         h[num]=i
+
+#     for i in range(n):
+#         for j in range(i+1, n):
+#             #list[a]+list[b]+list[c]=0
+#             #or list[c]=-list[a]-list[b]
+
+#             desired=-list[i]-list[j]
+
+#             if desired in h and h[desired]!=i and h[desired]!=j:
+#                 s.add(tuple(sorted([list[i],list[j],desired])))
+
+
+#     return s
+
+# nums = [-1,0,1,2,-1,-4]
+
+# print(three_sum(nums))
+
+
+#the above code is the best solution but you can perform the same operaton using sorted verson too
+
+
 def three_sum(list):
-    h={}
-    n=len(list)
-    s=set()
+    list.sort()
+    res=[]
 
-    for i , num in enumerate(list):
-        h[num]=i
+    for i in range(len(list)-2):
+        if i>0 and list[i]==list[i-1]:
+            continue
 
-    for i in range(n):
-        for j in range(i+1, n):
-            #list[a]+list[b]+list[c]=0
-            #or list[c]=-list[a]-list[b]
+        left,right=i+1,len(list)-1
 
-            desired=-list[i]-list[j]
+        while left<right:
 
-            if desired in h and h[desired]!=i and h[desired]!=j:
-                s.add(tuple(sorted([list[i],list[j],desired])))
+            total=list[i]+list[left]+list[right]
+
+            if total==0:
+                res.append([list[i],list[left],list[right]])
+
+                while left<right and list[left]==list[left+1]:
+                    left+=1
+                while left<right and list[right]==list[right-1]:
+                    right-=1
+
+                left+=1
+                right-=1
 
 
-    return s
+
+            elif total<0:
+                left+=1
+
+
+
+            else:
+                right-=1
+
+
+    return res
+
 
 nums = [-1,0,1,2,-1,-4]
 
 print(three_sum(nums))
+
