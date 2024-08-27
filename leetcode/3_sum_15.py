e20f5cc1f7568f1e6a72e1723d31db57bb81a95b
saugat <saugat556513@gmail.com>
2024-08-27 15:00:02 +0545

    day 13 of leetcoding

--- leetcode/3_sum_15.py
+++ leetcode/3_sum_15.py
@@ -27,46 +27,47 @@
 #the above code is the best solution but you can perform the same operaton using sorted verson too
 
 
-def three_sum(list):
-    list.sort()
-    res=[]
+# def three_sum(list):
+#     list.sort()
+#     res=[]
 
-    for i in range(len(list)-2):
-        if i>0 and list[i]==list[i-1]:
-            continue
+#     for i in range(len(list)-2):
+#         if i>0 and list[i]==list[i-1]:
+#             continue
 
-        left,right=i+1,len(list)-1
+#         left,right=i+1,len(list)-1
 
-        while left<right:
+#         while left<right:
 
-            total=list[i]+list[left]+list[right]
+#             total=list[i]+list[left]+list[right]
 
-            if total==0:
-                res.append([list[i],list[left],list[right]])
+#             if total==0:
+#                 res.append([list[i],list[left],list[right]])
 
-                while left<right and list[left]==list[left+1]:
-                    left+=1
-                while left<right and list[right]==list[right-1]:
-                    right-=1
+#                 while left<right and list[left]==list[left+1]:
+#                     left+=1
+#                 while left<right and list[right]==list[right-1]:
+#                     right-=1
 
-                left+=1
-                right-=1
-
-
-
-            elif total<0:
-                left+=1
+#                 left+=1
+#                 right-=1
 
 
 
-            else:
-                right-=1
+#             elif total<0:
+#                 left+=1
 
 
-    return res
+
+#             else:
+#                 right-=1
 
 
-nums = [-1,0,1,2,-1,-4]
+#     return res
 
-print(three_sum(nums))
 
+# nums = [-1,0,1,2,-1,-4]
+
+# print(three_sum(nums))
+
+#you can do either of the code, both are correct
