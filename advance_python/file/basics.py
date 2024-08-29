1c5cdfb168722c9a6a4d35e9950e05934594fd81
saugat <saugat556513@gmail.com>
2024-08-29 22:32:24 +0545

    some random program practise

--- advance_python/file/basics.py
+++ advance_python/file/basics.py
@@ -4,6 +4,13 @@ f.close()
 print("writting successful ")
 
 f=open("student.txt","r")
+print(f.readable())
 data=f.read()
 print(data)
 f.close()
+
+#f.readable()  to check if file can be read or not
+#f.writable()  to check if file can be written or not
+#f.closed      to check if the file is closed or not
+#f.mode        to know the mode of file (read or write)
+#f.name        to know the name of the file 
