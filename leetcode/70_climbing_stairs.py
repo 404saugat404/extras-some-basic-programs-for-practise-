6e6bef0bf432bb32883b02b2827a90acf8147258
saugat <saugat556513@gmail.com>
2024-09-03 23:18:35 +0545

    day 20 of leetcoding

--- leetcode/70_climbing_stairs.py
+++ leetcode/70_climbing_stairs.py
@@ -1,15 +1,35 @@
-class solutions:
-    def stairs(self,n:int)->int:
-        if n==1:
-            return 1
-        elif n==2:
-            return 2
+# class solutions:
+#     def stairs(self,n:int)->int:
+#         if n==1:
+#             return 1
+#         elif n==2:
+#             return 2
         
-        else:
-            return self.stairs(n-2)+self.stairs(n-1)
+#         else:
+#             return self.stairs(n-2)+self.stairs(n-1)
         
-value=solutions()
-print(value.stairs(3))
+# value=solutions()
+# print(value.stairs(3))
          
-this is the easiest way, but tc is O(2**n)
+# this is the easiest way, but tc is O(2**n)
 
+class solutions:
+    def stairs(self,n):
+        dict={
+            1:1,
+            2:2
+        }
+
+        def f(n):
+            if n in dict:
+                return dict[n]
+            
+            else:
+                dict[n]=f(n-2)+f(n-1)
+                return dict[n]
+
+        return f(n)
+    
+
+value=solutions()
+print(value.stairs(5))
