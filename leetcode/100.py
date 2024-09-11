3a72d7603d19597653f3b7287669aa80ed29168f
saugat <saugat556513@gmail.com>
2024-09-11 22:23:14 +0545

    day 25 of leetcoding

---
+++ leetcode/100.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p, q:):
+        if not p and not q: #if both null, return true
+            return True
+
+        if not p or not q: #if one is only null, return false
+            return False
+
+        if p.val!=q.val: #if numbers dont match, return false
+            return False
+
+        
+        return (self.isSameTree(p.left,q.left) and
+        self.isSameTree(p.right,q.right))
+        
