71d2b635d7988bb4cb7ee00e3e02b13690c188d5
saugat <saugat556513@gmail.com>
2024-09-13 00:01:55 +0545

    day 26 of leetcoding

---
+++ leetcode/125.py
@@ -0,0 +1,16 @@
+class solutions:
+    def valid_pal(string):
+        new_str=""
+
+        for s in string:
+            if s.isalnum():
+                new_str+=s
+
+        return new_str[:]==new_str[::-1]
+    
+
+
+abc=solutions
+
+value="race a car"
+print(abc.valid_pal(value))
